888733769481bcfdb418aae2285ff8a6d5484a26
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2023-02-06 16:11:22 +0100

    style: :wastebasket: delete coments on activity B2

--- 13a. Actividades DOM/actB2/actB2.js
+++ 13a. Actividades DOM/actB2/actB2.js
@@ -4,27 +4,6 @@ http://placekitten.com/g/200/300 usando DOM al ejercicioB2.html.
 Recuerda que para añadir el atributo (por ejemplo el src) a un elemento debes hacerlo de la
 siguiente forma:  nodoElemento.src = “http://prueba.com” */
 
-//var newElement = document.createElement("p");
-/* var elementText = "Hola Mundo!!";
-var content = document.createTextNode(elementText);
-var newImage = "http://placekitten.com/g/200/300";
-
-
-function addNewElement (element) {
-    return function addContent (content){
-        return document.body.appendChild(document.createElement(element.appendChild(content)));
-    }
-}
-newElement.appendChild(content);
-document.body.appendChild(newElement);
-
-
-const newParagraph = addNewElement("p");
-
-console.log(newParagraph("Hola Mundo")); */
-
-//const element = document.getElementsByTagName("body");
-
 function estilos(){
     var newParagraph = document.createElement("p");
     var content = document.createTextNode("Hola Mundo");
